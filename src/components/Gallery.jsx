f5733682095327b62c37ec346d194d109829d4cb
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-05-16 15:50:24 +0100

    adds isLoading and errorMessage

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,12 +1,18 @@
 import { Component } from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Alert, Card, Col, Container, Row, Spinner } from "react-bootstrap";
+import { FaFrown } from "react-icons/fa";
 
 class Gallery extends Component {
   state = {
-    movies: []
+    movies: [],
+    isLoading: true,
+    hasError: false,
+    errorMessage: ""
   };
 
   fetchMovies = async () => {
+    this.setState({ isLoading: true });
+
     try {
       const response = await fetch(`https://www.omdbapi.com/?s=${this.props.query}&apikey=9ee8febb`);
       if (response.ok) {
@@ -17,6 +23,9 @@ class Gallery extends Component {
       }
     } catch (error) {
       console.log(error);
+      this.setState({ hasError: true, errorMessage: error.message });
+    } finally {
+      this.setState({ isLoading: false });
     }
   };
 
@@ -28,6 +37,13 @@ class Gallery extends Component {
     return (
       <Container fluid className="px-lg-5 mt-3">
         <h4 className="text-white mb-3">{this.props.title}</h4>
+        {this.state.isLoading && <Spinner animation="border" variant="danger" />}
+        {this.state.hasError && (
+          <Alert variant="danger" className="d-flex align-items-center justify-content-center">
+            <FaFrown size={25} className="me-1" /> {this.state.errorMessage} <FaFrown size={25} className="ms-1" />
+          </Alert>
+        )}
+
         <Row className="g-2">
           {this.state.movies.map((movie) => (
             <Col xs={6} sm={6} md={4} lg key={movie.imdbID} className="mb-4">
